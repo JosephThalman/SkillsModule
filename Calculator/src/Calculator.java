7a1794f865d21ea56adbeb861883ad82efe237d5
JosephThalman <josephthalman@gmail.com>
2025-04-08 21:00:15 -0700

    Added project files.
    Added initial project files upon which the rest of the project will be built.

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,16 @@
+/**
+ *  Author:  Joseph Thalman
+ *  
+ *  Main program for the calculator application.
+*/
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		// Print welcome message
+		System.out.println("Welcome to the calculator!");
+
+	}
+
+}
